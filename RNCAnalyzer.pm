14ae94d85d9ced44a3fafd96bd9c2207eaf344a8
Sergey Romanov <complefor@rambler.ru>
2012-10-15 03:00:09 +0400

    Even more verbose output

--- RNCAnalyzer.pm
+++ RNCAnalyzer.pm
@@ -6,6 +6,7 @@ use open ':encoding(cp1251)';
 
 use Algorithm::Combinatorics qw(variations_with_repetition);
 use Data::Dumper;
+use List::MoreUtils qw(uniq);
 use List::Util qw(sum);
 use Log::Log4perl ();
 use Try::Tiny qw(try catch);
@@ -153,15 +154,44 @@ sub gather_stat_for_variation {
 
     my $stat;
     for my $ngram (@$ngrams) {
-        my $key;
+        my($key, $lex, @key_arr);
         for my $i (0..$#$var) {
             my $prop = $ngram->{$i}{$var->[$i]} || '_';
-            $key .= ($i == $lw ? "($prop)" : $prop).'++'
+            $key .= ($i == $lw ? "($prop)" : $prop).'++';
+            $key_arr[$i] = $i == $lw ? "($prop)" : $prop;
+            $lex->[$i] = $ngram->{$i}{lex} || '_';
         }
-        $stat->{ $key =~ s(\+\+$)()r }++
+        $stat->{ $key =~ s(\+\+$)()r }{num}++;
+        $stat->{ $key =~ s(\+\+$)()r }{key_arr} = \@key_arr;
+        push @{$stat->{ $key =~ s(\+\+$)()r }{lex}}, $lex;
     }
 
     return $stat;
+}
+
+sub stat_record {
+    my($struct, $variation, $lw) = @_;
+
+    my $line;
+    for my $i (0..$#$variation) {
+        $line .= $struct->{key_arr}[$i];
+        if (
+            $i == $lw
+            || $variation->[$i] =~ /lex/
+            || $struct->{key_arr}[$i] =~ /_/
+        ) {
+            $line .= '++';
+            next;
+        }
+        else {
+            $line .= '|';
+            my @lexs = uniq map {$_->[$i]} @{$struct->{lex}};
+            $line .= join ',', @lexs;
+            $line .= '++';
+        }
+    }
+
+    return $line =~ s(\+\+$)()r;
 }
 
 sub prepare_res {
@@ -171,12 +201,14 @@ sub prepare_res {
     $variation->[$lw] = "($variation->[$lw])";
     $res .= join '++', @$variation;
     $res .= "\n";
-    my @sorted_keys = sort {$stat->{$b} <=> $stat->{$a}} keys $stat;
+    my @sorted_keys = sort {$stat->{$b}{num} <=> $stat->{$a}{num}} keys $stat;
+    my $sum = sum map $stat->{$_}{num}, keys $stat;
     for my $i (0..$top-1) {
-        my $part = $stat->{$sorted_keys[$i]} * 100 / sum values $stat;
+        my $part = $stat->{$sorted_keys[$i]}{num} * 100 / $sum;
         $part =~ s!(?<=\.\d\d).*$!!;
-        $res .= $stat->{$sorted_keys[$i]}
-          ." ($part%)\t\t".$sorted_keys[$i]."\n";
+        $res .= $stat->{$sorted_keys[$i]}{num}
+          ." ($part%)\t\t"
+          .stat_record($stat->{$sorted_keys[$i]}, $variation, $lw)."\n";
     }
     $res .= "\n\n";
 
